e005c5f47c8867d7914c504345bca3a50c14d3f2
BartekPog <bartek.pogod@gmail.com>
2020-07-04 23:53:32 +0200

    Unify params style

--- modelcreator/machine.py
+++ modelcreator/machine.py
@@ -24,7 +24,7 @@ class Machine:
                 self.isClassifier = prev.isClassifier
                 self.isTrained = prev.isTrained
 
-    def learn(self, dataset_file, header_in_csv: bool = False, metrics=None, verbose: bool = True, cv: int = 3, computationLevel: str = 'medium'):
+    def learn(self, dataset_file: pd.DataFrame, header_in_csv: bool = False, metrics=None, verbose: bool = True, cv: int = 3, computation_level: str = 'medium'):
         dataset = pd.read_csv(dataset_file, header=(
             0 if header_in_csv else None))
 
@@ -32,17 +32,17 @@ class Machine:
         y = dataset.iloc[:, -1]
 
         self.learnFromDf(X=X, y=y, metrics=metrics, verbose=verbose,
-                         cv=cv, computationLevel=computationLevel)
+                         cv=cv, computation_level=computation_level)
 
-    def learnFromDf(self, X, y, metrics=None, verbose: bool = True, cv: int = 3, computationLevel: str = 'medium'):
+    def learnFromDf(self, X: pd.DataFrame, y: pd.DataFrame, metrics=None, verbose: bool = True, cv: int = 3, computation_level: str = 'medium'):
         self.isClassifier = isinstance(y[0], str)
 
         if(not isValidMetrics(metrics, self.isClassifier)):
             print("This metrics invalid")
             return None
 
-        if(computationLevel not in computationLevels):
-            print("{} is not a valid computation level".format(computationLevel))
+        if(computation_level not in computationLevels):
+            print("{} is not a valid computation level".format(computation_level))
             print("Valid computation levels:")
             for level in computationLevels:
                 print("\t{}".format(level))
@@ -52,14 +52,14 @@ class Machine:
         X_prep = self.transformer.fit_transform(X)
 
         modelData = generateModel(
-            X=X_prep, y=y, isClassification=self.isClassifier, metrics=metrics, verbose=verbose, cv=cv, computationLevel=computationLevel)
+            X=X_prep, y=y, isClassification=self.isClassifier, metrics=metrics, verbose=verbose, cv=cv, computationLevel=computation_level)
 
         self.modelName = modelData['name']
         self.model = modelData['estimator']
         self.modelParams = modelData['params']
         self.isTrained = True
 
-    def predict(self, features_file, output_file="output.csv", header_in_csv=False, verbose=True):
+    def predict(self, features_file: str, output_file="output.csv", header_in_csv=False, verbose=True):
         X_pred = pd.read_csv(features_file, header=(
             0 if header_in_csv else None))
 
@@ -69,7 +69,7 @@ class Machine:
         if(verbose):
             print("Results saved to ", output_file)
 
-    def predictFromDf(self, X_predictions, output_file=None, verbose=True):
+    def predictFromDf(self, X_predictions: pd.DataFrame, output_file=None, verbose=True):
         if not self.isTrained:
             print("Run learning function first")
             return pd.DataFrame({'err': True})
@@ -87,13 +87,7 @@ class Machine:
 
         return y_pred_dataframe
 
-    def learnAndPredict(self, train_set_file, prediction_features_file,
-                        output_file="output.csv", headers_in_csvs=False, metrics=None,
-                        verbose=True):
-        self.learn(train_set_file, headers_in_csvs, metrics, verbose)
-        self.predict(prediction_features_file, output_file, headers_in_csvs)
-
-    def saveMachine(self, output_file_name="machine.pkl"):
+    def saveMachine(self, output_file_name: str = "machine.pkl"):
         with open(output_file_name, 'wb') as file:
             joblib.dump(self, file)
             print("done")
