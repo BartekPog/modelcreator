6f62ba86a9590ff6c9ba558ae8f64267d8557d61
Bartłomiej Pogodziński <bartek.pogod@gmail.com>
2020-11-24 20:50:39 +0100

    Pin dependency versions (#15)
    * Pin dependency versions
    
    * Update gitignore
    
    * Update gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("readme.md", "r") as fh:
 
 setuptools.setup(
     name="modelcreator",
-    version="0.9.2",
+    version="0.9.3",
     author="Bartłomiej Pogodziński",
     author_email="bartek.pogod@gmail.com",
     description="Machine Learning package for quick fast model generation and comparison",
@@ -15,6 +15,6 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=["tests*"]),
     license='MIT',
     python_requires='>=3.6',
-    install_requires=['joblib', 'sklearn',
-                      'pandas', 'dask-ml', 'dask', 'imblearn'],
+    install_requires=['joblib==0.15.0', 'scikit-learn==0.23.1',
+                      'pandas==1.0.3', 'dask-ml==1.5.0', 'dask==2.19.0', 'imbalanced-learn==0.7.0'],
 )
